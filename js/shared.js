0e9bbcc8f78138c72a8fc6f85b1b0c3173edec17
TranHuy98 <huy8663@gmail.com>
2023-08-01 15:47:56 +0700

    add about page

---
+++ js/shared.js
@@ -0,0 +1,19 @@
+// const headerMenu = document.getElementsByClassName("header-content-menu-item");
+
+// for (let i = 0; i < headerMenu.length; i++) {
+//   headerMenu[i].onmouseenter = function () {
+//     let drop = this.children[0];
+//     console.log(drop);
+//     if (drop != undefined) {
+//       drop.style.display = "flex";
+//     }
+//   };
+
+//   headerMenu[i].onmouseover = function () {
+//     let drop = this.children[0];
+//     console.log(drop);
+//     if (drop != undefined) {
+//       drop.style.display = "none";
+//     }
+//   };
+// }
