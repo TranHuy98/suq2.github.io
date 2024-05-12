f3a13d5bf8c8fa73d80ac93d7aeb894a3a24285d
TranHuy98 <huy8663@gmail.com>
2024-05-12 22:44:43 +0700

    add send email

--- js/contact.js
+++ js/contact.js
@@ -1,12 +1,44 @@
 
-// gui thong tin form vao email
 
-function sendMail(){
-    let params = {
-        ten : document.getElementById("ten_kh").value,
-        email : document.getElementById("email_kh").value,
-        noidung : document.getElementById("noidung_kh").value,
-    }
+const sendMail = () => {
 
-    emailjs.send("service_tjqfgw5","template_l4ibfwe",params).then(alert("Success!"));
+  let name = document.getElementById("ten_kh").value;
+  let email = document.getElementById("email_kh").value;
+  let message = document.getElementById("noidung_kh").value;
+
+  if (name === '' || email === '' || message === '') {
+    alert('Vui lòng nhập đủ thông tin');
+  }
+  else {
+    const formatEmail = String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+    if (!formatEmail) {
+      alert('Email không đúng định dạng!');
+    } else {
+
+      const params = {
+        ten: name,
+        email: email,
+        noidung: message
+      }
+
+      console.log(params);
+
+      emailjs
+        .send("service_tjqfgw5", "template_l4ibfwe", params)
+        .then(alert("Gửi thông tin thành công!"));
+
+      document.getElementById('ten_kh').value = '';
+      document.getElementById('email_kh').value = '';
+      document.getElementById('noidung_kh').value = '';
+    };
+
+  }
 }
+
+
+
+
